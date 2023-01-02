a3e40ba6c307b9c31be3bb4c278c812b7f1b4f98
Juano <juano23200@gmail.com>
2023-01-02 17:28:36 -0500

    Fusiòn del menù en mobile

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+/*-----------CREACION DE SELECTORES PARA EL DESKTOPMENU----------------------- */
 //Permite seleccionar el elemento al cual se le va a dar click
 const menuEmail = document.querySelector('.navbar-email');
 
@@ -8,16 +10,37 @@ const desktopMenu = document.querySelector('.desktop-menu');//.desktop-menu perm
                                                             //podemos elegir cualquier clase
                                                             //que se encuentre dentro de este elemento
 
+
+/*-----------CREACION DE SELECTORES PARA EL MOBILEMENU----------------------- */
+//Permite seleccionar el elemento al cual se le va a dar click, en este caso la clase asignada al 
+//icono hamburguesa
+const menuHamIcon = document.querySelector('.menu');
+
+//Permite seleccionar el elemento el cual contiene la clase mobile-menu para poderla
+//utilizar dentro de la funcion toggleMobileMenu() la cual permite mostrar y 
+//ocultar el menu Hamburguesa.
+const mobileMenu = document.querySelector('.mobile-menu');
+
 //Permite escuchar un evento de tipo addEventListenner al moneto de darclick
 //sobre el elemento .navbar-email y se hace el llamado a la funcion toggleDesktopMenu() y asi
 //poder mostrar o ocultar el menu pequeño
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
+//Permite escuchar un evento de tipo addEventListenner al moneto de darclick
+//sobre el elemento .menu y se hace el llamado a la funcion toggleMobileMenu() y asi
+//poder mostrar o ocultar el menu hamburguesa
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+/*-----------FUNSION PARA MOSTRAR O OCULTAR EL MENU PEQUEÑO----------------------- */
 //funcion toggleDesktopMenu() para poder mostrar o ocultar el menu pequeño, que en este caso es la 
 //clase inactive
 function toggleDesktopMenu() {
   //console.log('Click');
   desktopMenu.classList.toggle('inactive');
 }
-
 //console.log('JS funcionando');
+
+/*-----------FUNSION PARA MOSTRAR O OCULTAR EL MENU HAMBURGUESA----------------------- */
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+  }
