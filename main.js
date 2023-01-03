1f6772e8e7f67ae1692eef5eb3fba21e55a9f553
Juano <juano23200@gmail.com>
2023-01-03 00:03:01 -0500

    Fusiòn del carrito de compras

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ const desktopMenu = document.querySelector('.desktop-menu');//.desktop-menu perm
                                                             //que se encuentre dentro de este elemento
 
 
+/*-----------CREACION DE SELECTORES PARA CUANDO SE LE DE CLICK SOBRE EL ICONO DEL CARRITO DE COMPPRAS----------------------- */                                                           
+//Permite seleccionar el elemento al cual se le va a dar click
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+//Permite seleccionar el elemento el cual contiene la clase product-detail para poderla
+//utilizar dentro de la funcion toggleDesktopMenu() la cual permite mostrar y 
+//ocultar el contenido del carrito de compras.
+const aside = document.querySelector('.product-detail');//.product-detail permite indicar que
+                                                            //podemos elegir cualquier clase
+                                                            //que se encuentre dentro de este elemento
+
 /*-----------CREACION DE SELECTORES PARA EL MOBILEMENU----------------------- */
 //Permite seleccionar el elemento al cual se le va a dar click, en este caso la clase asignada al 
 //icono hamburguesa
@@ -31,16 +42,44 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 //poder mostrar o ocultar el menu hamburguesa
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 
+//Permite escuchar un evento de tipo addEventListenner al moneto de darclick
+//sobre el elemento .product-detail y se hace el llamado a la funcion toggleMobileMenu() y asi
+//poder mostrar o ocultar el menu hamburguesa
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 /*-----------FUNSION PARA MOSTRAR O OCULTAR EL MENU PEQUEÑO----------------------- */
 //funcion toggleDesktopMenu() para poder mostrar o ocultar el menu pequeño, que en este caso es la 
 //clase inactive
 function toggleDesktopMenu() {
   //console.log('Click');
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+  
   desktopMenu.classList.toggle('inactive');
 }
 //console.log('JS funcionando');
 
 /*-----------FUNSION PARA MOSTRAR O OCULTAR EL MENU HAMBURGUESA----------------------- */
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+      aside.classList.add('inactive'); 
+    }
+    
     mobileMenu.classList.toggle('inactive');
+  }
+
+/*-----------FUNSION PARA MOSTRAR O OCULTAR EL CONTENIDO DEL CARRITO DE COMPRAS----------------------- */
+  function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive'); 
+    }
+    
+    aside.classList.toggle('inactive');
   }
